b8ae202665a6e493268b6747abcbd0109ade6593
Bach Nguyen <69bnguyen@gmail.com>
2025-01-19 18:28:21 +0700

    add dotfiles setup

--- init.sh
+++ init.sh
@@ -34,7 +34,49 @@
 devbox_dir="$HOME/Repo/personal/devbox"
 
 if [[ ! -d "$devbox_dir" ]]; then
+  echo "Creating devbox dir at '$devbox_dir'"
   mkdir -p "$devbox_dir"
 fi
 
 git clone "https://github.com/eeternalsadness/devbox.git" "$devbox_dir"
+
+echo "Creating symlink for 'devbox.json' at '$HOME/devbox.json'"
+ln -s "${devbox_dir}/devbox.json" "$HOME/devbox.json"
+
+# NOTE: update the values in the .env file as necessary
+echo "Copying 'template.env' to '$HOME/.env'"
+cp "${devbox_dir}/template.env" "$HOME/.env"
+
+##############################################
+# Set up dotfiles
+##############################################
+
+# NOTE: change this as necessary
+dotfiles_dir="$HOME/Repo/personal/dotfiles"
+
+if [[ ! -d "$dotfiles_dir" ]]; then
+  echo "Creating dotfiles dir at '$dotfiles_dir'"
+  mkdir -p "$dotfiles_dir"
+fi
+
+git clone "https://github.com/eeternalsadness/dotfiles.git" "$dotfiles_dir"
+
+# Symlink dotfiles
+echo "Linking dotfiles..."
+cd "$dotfiles_dir"
+
+for file in .*; do
+  # Ignore special files
+  if [[ "$file" == "." || "$file" == ".." || "$file" == ".git" || "$file" == ".gitignore" ]]; then
+    continue
+  fi
+
+  target="$HOME/$file"
+
+  # Create symlink
+  echo "Creating symlink for '$file' at '$target'"
+  ln -s "$dotfiles_dir/$file" "$target"
+done
+
+echo "Reminders:"
+echo "- Modify the values in '$HOME/.env' as necessary"
