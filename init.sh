078e555f8c2934f40b094528197c6ec407c36461
Bach Nguyen <69bnguyen@gmail.com>
2025-01-20 18:58:30 +0700

    update template.env & trim init script

--- init.sh
+++ init.sh
@@ -1,31 +1,5 @@
 #!/usr/bin/env bash
 
-#pkg_manager_cmd="apt"
-#
-#if [ "$(uname -s)" = "Darwin" ]; then
-#  # install brew if not installed
-#  if ! [ brew --version ] &>/dev/null; then
-#    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#  fi
-#  pkg_manager_cmd="brew"
-#elif [ -f /etc/os-release ]; then
-#  local linux_distro=$(grep "^ID=" /etc/os-release | sed 's/^ID=//')
-#  case "$linux_distro" in
-#  ubuntu)
-#    pkg_manager_cmd="apt"
-#    ;;
-#  *)
-#    pkg_manager_cmd="apt"
-#    ;;
-#  esac
-#else
-#  echo "Unknown Operating System!"
-#  exit 1
-#fi
-#
-#install_common_packages() {
-#}
-
 ##############################################
 # Set up devbox
 ##############################################
